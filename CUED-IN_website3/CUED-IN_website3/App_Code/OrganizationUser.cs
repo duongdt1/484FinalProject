26106c9ca26a3d6ded1e8539ef0085a8135d08d3
mhueskes <44989130+mhueskes@users.noreply.github.com>
2019-03-24 17:16:26 -0400

    Add files via upload

---
+++ CUED-IN_website3/CUED-IN_website3/App_Code/OrganizationUser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for OrganizationUser
+/// </summary>
+public class OrganizationUser
+{
+    String userName;
+    String password;
+    String email;
+   
+    DateTime lastUpdated;
+    String lastUpdatedBy;
+
+    public OrganizationUser(String userName, String password, String email)
+    {
+        setUserName(userName);
+        setEmail(email);
+        setPassword(password);
+       
+        setLastUpdatedBy();
+        setLastUpdated();
+        
+    }
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+   
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+   
+    public void setLastUpdated()
+    {
+        this.lastUpdated = DateTime.Today;
+    }
+    public void setLastUpdatedBy()
+    {
+        this.lastUpdatedBy = "Blueberry";
+    }
+   
+    public String getEmail()
+    {
+        return email;
+    }
+    public DateTime getLastUpdated()
+    {
+        return lastUpdated;
+    }
+    public String getLastUpdatedBy()
+    {
+        return lastUpdatedBy;
+    }
+    public String getUserName()
+    {
+        return userName;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+    
+}
