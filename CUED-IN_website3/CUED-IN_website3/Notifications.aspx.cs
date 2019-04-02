b10e9f4e4797311e8c8e3fddb649f5b16288b66d
edc3322 <44984066+edc3322@users.noreply.github.com>
2019-04-02 16:48:48 -0400

    Add files via upload

--- CUED-IN_website3/CUED-IN_website3/Notifications.aspx.cs
+++ CUED-IN_website3/CUED-IN_website3/Notifications.aspx.cs
@@ -4,11 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
 
 public partial class Notifications : System.Web.UI.Page
 {
+    OrganizationUser signedInUser;
     protected void Page_Load(object sender, EventArgs e)
     {
+        signedInUser = (OrganizationUser)Session["User"];
+        populateGrdNotification();
+    }
 
+    public void populateGrdNotification()
+    {
+        using (SqlConnection connection = connect()) // finds the title for the selected job using jobID
+        {
+            SqlCommand select = new SqlCommand();
+            select.CommandText = "SELECT * FROM Notification WHERE UserName = @Username";
+            select.Connection = connection;
+            select.Parameters.AddWithValue("@Username", signedInUser.getUserName());
+            connection.Open();
+            select.ExecuteNonQuery();
+            SqlDataReader cursor = select.ExecuteReader();
+
+            grdNotifications.DataSource = cursor;
+            grdNotifications.DataBind();
+        }
+
+    }
+    public SqlConnection connect()
+    {
+        SqlConnection dbConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+
+        return dbConnect;
     }
 }
