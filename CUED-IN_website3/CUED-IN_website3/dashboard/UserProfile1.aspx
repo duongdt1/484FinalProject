35b6d65e186e4753538757896c74dd2523da856b
duongdt1 <45021664+duongdt1@users.noreply.github.com>
2019-04-17 21:38:28 -0400

    User Profile with UI

---
+++ CUED-IN_website3/CUED-IN_website3/dashboard/UserProfile1.aspx
@@ -0,0 +1,135 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/dashboard/CUEDINMaster.master" AutoEventWireup="true" CodeFile="UserProfile1.aspx.cs" Inherits="dashboard_UserProfile1" MaintainScrollPositionOnPostback="true"%>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
+    <div id="page-wrapper" >
+            <div id="page-inner">
+			 <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                            User Profile <small></small>
+                        </h1>
+                    </div>
+                </div> 
+                 <!-- /. ROW  -->
+              <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                        <div class="form-group">
+         
+       <!-- Form with ASP tags start here -->
+
+    <div class="row">
+                <div class="col-lg-12">
+                  <div class="panel panel-default">
+                        <div class="panel-heading">
+                          
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-6">
+    
+     <form id="form1" >
+        <div>
+            <asp:Panel ID="Panel1" runat="server">
+                <asp:Label ID="Label1" runat="server" Text="Profile" Width="150px"></asp:Label>
+                <asp:LinkButton ID="btnEditOrg" runat="server" OnClick="btnEditOrg_Click">Edit</asp:LinkButton>
+                <br />
+                <br />
+                <asp:Label ID="Label2" runat="server" Text="Organization Name"></asp:Label>
+                <br />
+                <asp:Label ID="lblOrgName" runat="server" Text="Label"></asp:Label>
+                <asp:TextBox class="form-control" ID="txtOrgName" runat="server" Visible="False"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Label ID="Label3" runat="server" Text="Organization Type"></asp:Label>
+                <br />
+                <asp:Label ID="lblOrgType" runat="server" Text="Label"></asp:Label>
+                <asp:RadioButtonList ID="radOrgType" runat="server" Visible="False">
+                    <asp:ListItem>Profit</asp:ListItem>
+                    <asp:ListItem>Non-Profit</asp:ListItem>
+                </asp:RadioButtonList>
+                <br />
+                <br />
+                <asp:Label ID="Label11" runat="server" Text="Primary Career Cluster"></asp:Label>
+                <br />
+                <asp:Label ID="lblCareerCluster" runat="server" Text="lblCareerCluster"></asp:Label>
+                <asp:ListBox ID="lstCareerCluster" runat="server" Visible="False"></asp:ListBox>
+                <br />
+                <br />
+                <asp:Label ID="Label6" runat="server" Text="Phone Number"></asp:Label>
+                <br />
+                <asp:Label ID="lblOrgPhoneNumber" runat="server" Text="lblOrgPhoneNumber"></asp:Label>
+                <br />
+                <asp:TextBox class="form-control" ID="txtOrgPhone" runat="server" Visible="False"></asp:TextBox>
+                <br />
+            </asp:Panel>
+        </div>
+        <asp:Panel ID="Panel2" runat="server">
+            <asp:Label ID="Label8" runat="server" Text="User Information" Width="150px"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label9" runat="server" Text="Username"></asp:Label>
+            <br />
+            <asp:Label ID="lblUsername" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <asp:LinkButton ID="btnEditPassword" runat="server" OnClick="btnEditPassword_Click">Change Password</asp:LinkButton>
+            <br />
+            <br />
+            <asp:Label ID="lblPass" runat="server" Text="Enter Password" Visible="False"></asp:Label>
+            <br />
+            <asp:TextBox class="form-control" ID="txtPass" runat="server" Visible="False"></asp:TextBox>
+            <br />
+            <asp:Label ID="lblPassConfirm" runat="server" Text="Re-enter Password" Visible="False"></asp:Label>
+            <br />
+            <asp:TextBox class="form-control" ID="txtPassConfirm" runat="server" Visible="False"></asp:TextBox>
+            <br />
+            <asp:CompareValidator ID="CompareValidator1" runat="server" ControlToCompare="txtPass" ControlToValidate="txtPassConfirm" ErrorMessage="Passwords do not match"></asp:CompareValidator>
+            <br />
+            <asp:Button class="btn btn-dark" ID="btnSave" runat="server" OnClick="btnSave_Click" Text="Save" Visible="False" />
+            <asp:Button class="btn btn-dark" ID="btnCancel" runat="server" OnClick="btnCancel_Click" Text="Cancel" Visible="False" />
+            <br />
+            <br />
+            <asp:Label ID="Label10" runat="server" Text="Notifications"></asp:Label>
+            <br />
+            <asp:RadioButtonList ID="radNotifications" runat="server" OnSelectedIndexChanged="radNotifications_SelectedIndexChanged" AutoPostBack="true">
+                <asp:ListItem>Enabled</asp:ListItem>
+                <asp:ListItem>Disabled</asp:ListItem>
+            </asp:RadioButtonList>
+        </asp:Panel>
+    </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        </div>
+
+        <!-- Form with ASP tags end here -->
+
+                                    </div>
+                                </div>
+                                <!-- /.col-lg-6 (nested) -->
+                            </div>
+                            <!-- /.row (nested) -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+			<footer><p></p></footer>
+			</div>
+             <!-- /. PAGE INNER  -->
+            </div>
+</asp:Content>
+
---
+++ CUED-IN_website3/CUED-IN_website3/dashboard/UserProfile1.aspx.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+public partial class dashboard_UserProfile1 : System.Web.UI.Page
+{
+    public static bool chgPass; //handles the controls for when a user is changing passwords
+    OrganizationUser signedInUser;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        signedInUser = (OrganizationUser)Session["User"];
+        populateLstCareerCluster();
+        if (!IsPostBack)
+        {
+            setOrgValues();
+            chgPass = true;
+        }
+    }
+
+    public SqlConnection connect()
+    {
+        SqlConnection dbConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+
+        return dbConnect;
+    }
+
+    protected void btnEditOrg_Click(object sender, EventArgs e)
+    {
+        if (btnEditOrg.Text == "Edit")
+        {
+            btnEditOrg.Text = "Save";
+            editMode(true);
+        }
+        else
+        {
+            btnEditOrg.Text = "Edit";
+            editMode(false);
+            //if the save button is pressed, update the database
+            using (SqlConnection connection = connect())
+            {
+                connection.Open();
+                SqlCommand update = new SqlCommand();
+                update.Connection = connection;
+                update.CommandText = "UPDATE Organization SET OrganizationName = @OrgName, OrganizationType = @OrgType, CareerCluster = @CareerCluster, PhoneNumber = @PhoneNumber, LastUpdated = GETDATE() WHERE OrganizationID = @OrgID";
+                update.Parameters.AddWithValue("@OrgName", txtOrgName.Text);
+                update.Parameters.AddWithValue("@OrgType", radOrgType.SelectedValue);
+                update.Parameters.AddWithValue("@CareerCluster", lstCareerCluster.SelectedValue);
+                update.Parameters.AddWithValue("@PhoneNumber", txtOrgPhone.Text);
+                update.Parameters.AddWithValue("@OrgID", signedInUser.getOrgID());
+                update.ExecuteNonQuery();
+            }
+            setOrgValues();
+        }
+    }
+
+    protected void btnEditPassword_Click(object sender, EventArgs e)
+    {
+        editPassword(chgPass);
+    }
+
+    public void editMode(bool edit)//toggles the page controls for an organization based on the mode
+    {
+        if (edit)
+        {
+            lblOrgName.Visible = false;
+            lblCareerCluster.Visible = false;
+            lblOrgType.Visible = false;
+            txtOrgName.Visible = true;
+            radOrgType.Visible = true;
+            lstCareerCluster.Visible = true;
+            txtOrgPhone.Visible = true;
+        }
+        else
+        {
+            lblOrgName.Visible = true;
+            lblCareerCluster.Visible = true;
+            lblOrgType.Visible = true;
+            txtOrgName.Visible = false;
+            radOrgType.Visible = false;
+            lstCareerCluster.Visible = false;
+            txtOrgPhone.Visible = false;
+        }
+    }
+
+    public void editPassword(bool edit)
+    {
+        if (edit)
+        {
+            btnEditPassword.Visible = false;
+            lblPass.Visible = true;
+            lblPassConfirm.Visible = true;
+            txtPass.Visible = true;
+            txtPassConfirm.Visible = true;
+            btnCancel.Visible = true;
+            btnSave.Visible = true;
+            chgPass = false;
+        }
+        else
+        {
+            btnEditPassword.Visible = true;
+            lblPass.Visible = false;
+            lblPassConfirm.Visible = false;
+            txtPass.Visible = false;
+            txtPassConfirm.Visible = false;
+            btnCancel.Visible = false;
+            btnSave.Visible = false;
+            txtPassConfirm.Text = "";
+            txtPass.Text = "";
+            chgPass = true;
+        }
+    }
+    public void setOrgValues()//sets the values of the controls in the form with values from the database
+    {
+        using (SqlConnection connection = connect())
+        {
+            SqlCommand select = new SqlCommand();
+            select.Connection = connection;
+            select.CommandText = "SELECT OrganizationType, OrganizationName, CareerCluster, PhoneNumber FROM Organization WHERE OrganizationID = @OrgID";
+            select.Parameters.AddWithValue("@OrgID", signedInUser.getOrgID());
+            connection.Open();
+            SqlDataReader cursor = select.ExecuteReader();
+            while (cursor.Read())
+            {
+                lblOrgType.Text = cursor[0].ToString();
+                radOrgType.SelectedValue = cursor[0].ToString();
+                lblOrgName.Text = cursor[1].ToString();
+                txtOrgName.Text = cursor[1].ToString();
+                lblCareerCluster.Text = cursor[2].ToString();
+                lstCareerCluster.SelectedValue = cursor[2].ToString();
+                lblOrgPhoneNumber.Text = cursor[3].ToString();
+                txtOrgPhone.Text = cursor[3].ToString();
+            }
+        }
+        using (SqlConnection connection = connect())
+        {
+            SqlCommand select = new SqlCommand();
+            select.Connection = connection;
+            connection.Open();
+            select.CommandText = "SELECT Username, Notifications FROM OrganizationUser WHERE Username = @Username";
+            select.Parameters.AddWithValue("@Username", signedInUser.getUserName());
+            SqlDataReader cursor = select.ExecuteReader();
+            while (cursor.Read())
+            {
+                lblUsername.Text = cursor[0].ToString();
+                radNotifications.SelectedValue = cursor[1].ToString();
+            }
+        }
+    }
+    public void populateLstCareerCluster()
+    {
+        //populates the list box of career cluster items
+        using (SqlConnection connection = connect())
+        {
+            connection.Open();
+            SqlCommand select = new SqlCommand();
+            select.Connection = connection;
+            select.CommandText = "SELECT * FROM CareerCluster";
+            SqlDataReader cursor = select.ExecuteReader();
+            while (cursor.Read())
+            {
+                lstCareerCluster.Items.Add(cursor[0].ToString());
+            }
+        }
+    }
+
+    protected void radNotifications_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        using (SqlConnection connection = connect())
+        {
+            SqlCommand update = new SqlCommand();
+            update.Connection = connection;
+            connection.Open();
+            update.CommandText = "UPDATE OrganizationUser SET Notifications = @Notifications WHERE Username = @Username";
+            update.Parameters.AddWithValue("@Notifications", radNotifications.SelectedValue);
+            update.Parameters.AddWithValue("@Username", signedInUser.getUserName());
+            update.ExecuteNonQuery();
+        }
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e)//change the password
+    {
+        using (SqlConnection connection = connect())
+        {
+            SqlCommand update = new SqlCommand();
+            update.Connection = connection;
+            connection.Open();
+            update.CommandText = "UPDATE OrganizationUser SET Password = @Password WHERE Username = @Username";
+            update.Parameters.AddWithValue("@Password", PasswordHash.HashPassword(txtPass.Text));
+            update.Parameters.AddWithValue("@Username", signedInUser.getUserName());
+            update.ExecuteNonQuery();
+        }
+        editPassword(chgPass);
+    }
+
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        editPassword(chgPass);
+    }
+}
